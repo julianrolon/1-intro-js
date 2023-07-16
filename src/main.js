21f73448115c03d196aa7f75f8da2323e813cc2d
julianrolon <julianrolonramirez@gmail.com>
2023-07-16 12:09:04 -0500

    Variables and constants

---
+++ src/main.js
@@ -0,0 +1,16 @@
+console.log('tons que');
+
+let name = 'Pepe';
+// name = 'Andres'
+
+const lastname = 'Doe';
+
+const condicion = true;
+
+if(condicion){
+    name = 'Ale';
+    const lastname = 'Doe';
+    console.log('Inside ' lastname )
+}
+
+console.log('Hallo mondo ' + name + ' ' + lastname )
