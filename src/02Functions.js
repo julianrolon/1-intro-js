e8369b69c7ae5498436b2db9822a5c1e1846cee8
julianrolon <julianrolonramirez@gmail.com>
2023-07-31 16:45:54 -0500

    02Functions

---
+++ src/02Functions.js
@@ -0,0 +1,19 @@
+function sayHello(name = 'Pepe'){
+    console.log(`Hallo Function ${name}`);
+}
+
+function sayHelloReturn(){
+    const greeting = 'Hallo Function R';
+    return greeting;
+}
+
+const sayHolaLambda = (name = 'Default') => 'Hallo Function Lambda ' + name;
+const add = (a = 0, b = 0) => a + b;
+
+sayHello();
+sayHello('Carlitos');
+const result = sayHelloReturn();
+console.log(result);
+
+console.log(sayHolaLambda());
+console.log(add(123, 77));
